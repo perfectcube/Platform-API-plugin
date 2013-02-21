76dac5ec122a820d2d1ddd614e1de71109938ed2
Andy Dawson <anda@nodes.dk>
2013-02-21 21:55:32 +0000

    don't need the word "Error" in there

--- Error/Exception/ValidationException.php
+++ Error/Exception/ValidationException.php
@@ -42,7 +42,7 @@ class ValidationException extends CakeException {
 					continue;
 				}
 
-				$this->message = sprintf('%s.%s Error : %s', $model, $field, $error);
+				$this->message = sprintf('%s.%s : %s', $model, $field, $error);
 				if (!empty($rule['code'])) {
 					$code = $rule['code'];
 				}
