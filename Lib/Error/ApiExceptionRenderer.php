33497b51ad9b956b2691a085c0bf3b70d34c2ee7
Andy Dawson <anda@nodes.dk>
2013-02-21 17:08:15 +0000

    add validation exception
    permit defining the response code in the validate array
    
    if there's only one validation error - modify the response so that it's
    obvious there wsa only one error.

--- Lib/Error/ApiExceptionRenderer.php
+++ Lib/Error/ApiExceptionRenderer.php
@@ -4,6 +4,29 @@ App::uses('ExceptionRenderer', 'Error');
 class ApiExceptionRenderer extends ExceptionRenderer {
 
 /**
+ * Renders validation errors and sends a 412 error code
+ *
+ * @param ValidationErrorException $error
+ * @return void
+ **/
+	public function validation($error) {
+		$url = $this->controller->request->here();
+		$code = $error->getCode();
+		$status = ($code >= 400 && $code < 506) ? $code : 412;
+		$this->controller->response->statusCode($status);
+		$this->controller->set(array(
+			'code' => $code,
+			'url' => h($url),
+			'name' => $error->getMessage(),
+			'validationErrors' => $error->getValidationErrors(),
+			'error' => $error,
+			'_serialize' => array('code', 'url', 'name', 'validationErrors')
+		));
+
+		$this->_outputMessage('error400');
+	}
+
+/**
  * Generate the response using the controller object.
  *
  * If there is no specific template for the error raised (normally there will
